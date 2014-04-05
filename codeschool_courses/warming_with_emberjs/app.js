86c374f37280583d9c6465c6c065abaaffb62199
budhram gurung <budhram@vertisinfotech.com>
2014-04-05 16:39:08 +0530

    Emberjs: Changing route to custome path

--- codeschool_courses/warming_with_emberjs/app.js
+++ codeschool_courses/warming_with_emberjs/app.js
@@ -2,10 +2,7 @@ var App = Ember.Application.create({
   LOG_TRANSITIONS: true   // log to the console when visiting a new page
 });
 
-App.Router.map(function(){
-  this.route('about')
-});
-
-App.Router.map(function(){
-  this.route('credits')
+App.Router.map(function() {
+  this.route('credits', { path: '/thanks' });
+  this.route('about');
 });
