d13ad4d2d79c5b2ab06458fd9d134322ed51e2ad
budhram gurung <budhram@vertisinfotech.com>
2014-04-05 16:06:37 +0530

    Emberjs: Added credits page & templates

--- codeschool_courses/warming_with_emberjs/app.js
+++ codeschool_courses/warming_with_emberjs/app.js
@@ -1,3 +1,11 @@
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true   // log to the console when visiting a new page
 });
+
+App.Router.map(function(){
+  this.route('about')
+});
+
+App.Router.map(function(){
+  this.route('credits')
+});
