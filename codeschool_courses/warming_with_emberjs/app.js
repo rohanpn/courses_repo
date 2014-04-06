2e806bd631003382b3c3170a9af4da45752f1a8d
budhram gurung <budhram@vertisinfotech.com>
2014-04-06 13:51:27 +0530

    Emberjs: Updated for products/contacts fixture data

--- codeschool_courses/warming_with_emberjs/app.js
+++ codeschool_courses/warming_with_emberjs/app.js
@@ -6,42 +6,87 @@ Handlebars.registerHelper("log", function(context) {
   return console.log(context);
 });
 
-App.PRODUCTS = [
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string'),
+  reviews: DS.hasMany('review', { async: true }),
+  crafter: DS.belongsTo('contact')
+});
+App.Product.FIXTURES = [
   {
+    id: 1,
+    crafter: 200,
     title: 'Chair',
     price: 99,
     description: 'Chair is...',
     isOnSale: true,
-    image: 'images/products/chair.jpg'
+    image: 'images/products/chair.jpg',
+    reviews: [100, 101]
   },
   {
+    id: 2,
+    crafter: 201,
     title: 'Old Clock',
     price: 59,
     description: 'Clock is...',
     isOnSale: false,
-    image: 'images/products/clock.jpg'
+    image: 'images/products/clock.jpg',
+    reviews: []
   }
 ];
-App.CONTACTS = [
+
+App.Contact = DS.Model.extend({
+  name: DS.attr('string'),
+  about: DS.attr('string'),
+  avatar: DS.attr('string'),
+  products: DS.hasMany('product', { async: true })
+});
+App.Contact.FIXTURES = [
   {
+    id: 200,
+    products: [1, 2],
     name: 'Man Kumari',
     about: 'Sales and Marketing',
-    avatar: 'images/contacts/man.jpg'
+    avatar: 'images/contacts/man.jpg',
   },
   {
+    id: 201,
+    products: [2],
     name: 'Budh Ram',
     about: 'Support and Issues',
     avatar: 'images/contacts/ram.jpg'
   }
 ];
 
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date'),
+  product: DS.belongsTo('product')
+});
+App.Review.FIXTURES = [
+  {
+    id: 100,
+    product: 1,
+    text: "Started a fire in no time!"
+  },
+  {
+    id: 101,
+    product: 1,
+    text: "Not the brightest flame, but warm!"
+  }
+];
+
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.resource('products', function(){
-    this.resource('product', { path: '/:title'});
+    this.resource('product', { path: '/:product_id' });
   });
   this.resource('contacts', function(){
-    this.resource('contact', { path: '/:name'});
+    this.resource('contact', { path: '/:contact_id'});
   });
 });
 
@@ -61,23 +106,23 @@ App.ContactsIndexController = Ember.Controller.extend({
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-    return App.PRODUCTS;
+    return this.store.findAll('product');
   }
 });
 App.ProductRoute = Ember.Route.extend({
   model: function(params) {
     console.log(params);
-    return App.PRODUCTS.findBy('title', params.title);
+    return this.store.find('product', params.product_id);
   }
 });
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
-    return App.CONTACTS;
+    return this.store.findAll('contact');
   }
 });
 App.ContactRoute = Ember.Route.extend({
   model: function(params) {
     console.log(params);
-    return App.CONTACTS.findBy('name', params.name);
+    return this.store.find('contact', params.contact_id);
   }
 });
