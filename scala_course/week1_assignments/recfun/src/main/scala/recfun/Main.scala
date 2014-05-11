3a2bb8ecfa74718a5805de3ba65f8c2759237668
budhram gurung <budhram@vertisinfotech.com>
2014-05-11 13:32:20 +0530

    Scala Course: Week1 assignments

---
+++ scala_course/week1_assignments/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,49 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+	else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def check_balance(chars: List[Char], acc: Int): Boolean = {
+	  if (chars.isEmpty) (acc == 0)
+	  else if (acc >= 0) {
+	    if ('('.equals(chars.head)) check_balance(chars.tail, acc + 1)
+	    else if (')'.equals(chars.head)) check_balance(chars.tail, acc - 1)
+	    else check_balance(chars.tail, acc)
+	  } 
+	  else false
+	}
+	check_balance(chars, 0)    
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0) 1
+    else if (money < 0) 0
+    else if (coins.isEmpty) 0
+    else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+}
+}
