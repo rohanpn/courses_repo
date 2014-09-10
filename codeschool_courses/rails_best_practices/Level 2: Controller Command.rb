b190c588f024b4b263041ef4b093d9bc8b23d59d
Budhram Gurung <budhram.gurung01@gmail.com>
2014-09-10 16:17:57 +0530

    Codeschool Rails Best Practices : Updated Level 2 notes

--- codeschool_courses/rails_best_practices/Level 2: Controller Command.rb
+++ codeschool_courses/rails_best_practices/Level 2: Controller Command.rb
@@ -5,6 +5,33 @@ begin
   # Task Code
   # app/models/tweet.rb
   class Tweet < ActiveRecord::Base
+    has_one :location, :dependent => :destroy
+  end
+  # app/controllers/tweets_controller.rb
+  class TweetsController < ApplicationController
+    def new
+      @tweet = current_user.tweets.build
+      @location = Location.new
+    end
+
+    def create
+      @tweet = current_user.tweets.build(params[:tweet])
+      @location = Location.new(params[:location])
+
+      if @tweet.save
+        @location.save
+        @tweet.location = @location
+
+        redirect_to @tweet, :notice => 'Successfully created a Tweet'
+      else
+        render :new
+      end
+    end
+  end
+
+  # Solution Code
+  # app/models/tweet.rb
+  class Tweet < ActiveRecord::Base
     has_one :location, dependent: :destroy
     accepts_nested_attributes_for :location
   end
@@ -24,9 +51,6 @@ begin
       end
     end
   end
-
-  # Solution Code
-
 end
 
 # ==========================================================================>
